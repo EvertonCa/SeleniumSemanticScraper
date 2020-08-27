e319f47ce28c1e8b55c99cdb7d513dca17593fab
Weverson <unifwpereira@fei.edu.br>
2020-08-26 23:08:45 -0300

    Header's xpath change

--- SemanticScholarMetaCrawler.py
+++ SemanticScholarMetaCrawler.py
@@ -122,7 +122,7 @@ class Crawler:
                     "//button[@data-selenium-selector='last-five-years-filter-button']")
                 driver.execute_script('arguments[0].click()', element)
                 driver.find_element_by_xpath(
-                    "//div[@class='flex-container flex-row-vcenter dropdown-filters__flex-container']").click()
+                    "//div[@class='flex-container flex-row-vcenter dropdown-filters__outer-flex-container']").click()
             elif k == 2:  # results with Reviews marked
                 driver.find_element_by_xpath(
                     "//button[@class='cl-button cl-button--no-arrow-divider cl-button--not-icon-only cl-button--no-icon cl-button--has-label cl-button--icon-pos-left cl-button--shape-rectangle cl-button--size-default cl-button--type-default cl-dropdown-button cl-dropdown dropdown-filters__pub_type']").click()
@@ -130,7 +130,7 @@ class Crawler:
                     "//section[@data-selenium-selector='publicationType']").click()
                 driver.find_element_by_xpath("//*[contains(text(), 'Review (')]").click()
                 driver.find_element_by_xpath(
-                    "//div[@class='flex-container flex-row-vcenter dropdown-filters__flex-container']").click()
+                    "//div[@class='flex-container flex-row-vcenter dropdown-filters__outer-flex-container']").click()
             else:
                 pass
 
